4ca922e1c80f0d882f6bce10275241307aadcfa3
Keira Pierce <68132654+kpierce11@users.noreply.github.com>
2025-05-14 00:44:17 -0400

    Updated DiceRoller.tsx

--- src/pages/DiceRoller.tsx
+++ src/pages/DiceRoller.tsx
@@ -348,16 +348,7 @@ export const DiceRoller: React.FC = () => {
   
   <div ref={diceContainerRef}
        id="dice-box-container"
-       style={{ 
-         width: '100%', 
-         height: '100%', 
-         position: 'absolute',
-         top: 0,
-         left: 0,
-         right: 0,
-         bottom: 0,
-         overflow: 'hidden'
-       }}
+       style={{ width: '100%', height: '100%', position: 'relative' }}
   />
 </div>
         
